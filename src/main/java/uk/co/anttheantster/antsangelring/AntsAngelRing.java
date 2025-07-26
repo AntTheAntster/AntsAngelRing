72bdb1c8d1b74256e90da1e31333ed3a174dfb13
AntTheAntster <anttheantster@gmail.com>
2025-07-26 17:28:48 +0100

    Updated version

--- src/main/java/uk/co/anttheantster/antsangelring/AntsAngelRing.java
+++ src/main/java/uk/co/anttheantster/antsangelring/AntsAngelRing.java
@@ -30,7 +30,7 @@ public class AntsAngelRing {
     public boolean accessoriesLoaded;
 
     public static final String MOD_ID = "antsangelring";
-    public static final String version = "1.2.1.2";
+    public static final String version = "1.2.1.3";
 
     private static AngelRingSettings angelRingSettings;
 
